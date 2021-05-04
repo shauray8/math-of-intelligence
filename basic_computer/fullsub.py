74aaa1539932b4428dd0d36086abeabf58ef5775
Shauray <shauray9@gmail.com>
2021-05-04 23:36:33 +0530

    logic gate stuff

---
+++ basic_computer/fullsub.py
@@ -0,0 +1,18 @@
+# full subtractor in loginc gates
+
+a = 1
+b = 0
+
+def sub(a, b, c):
+    first_xor = a ^ b
+    a = ~a
+    first_and = a & b
+    diff = first_xor ^ c
+    first_xor = ~first_xor
+    second_and = c & first_xor
+    b_out = first_and & second_and
+
+    print(b_out)
+
+
+sub(a,b,0)
