79ceecd911772c429a921dde19c9987dee6a0eb7
Shauray <shauray9@gmail.com>
2021-11-11 18:01:24 +0530

    past commits

--- machine_learning/k_nearest_neighbours.py
+++ machine_learning/k_nearest_neighbours.py
@@ -1,1 +1,15 @@
+#bin/bash
+import numpy 
+
+## ----------------- nearest neighbour classifier ----------------- ##
+
+def near_neigh():
+    pass
+
+## ----------------- K nearest neighbour classifier ----------------- ##
+
+def K_near_neigh():
+    pass
+
+
 
