92c586dd82b9c8576ba9c33f2a28a935717b32e4
Shauray <shauray9@gmail.com>
2022-07-02 20:37:56 +0530

    cleaning blur filters

--- cv_stuff/blur.py
+++ cv_stuff/blur.py
@@ -5,6 +5,7 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import cv2
 
 test_image = np.random.rand(5,5)
 def conv(image,kernel):
@@ -22,11 +23,16 @@ def conv(image,kernel):
 
     return image_size, blured
 
+### Getting it done as soon as I get some time !!!
 def padding():
     pass
 
+def Gaussian(kernel = 3):
+    pass
+###
+
 def image_processing():
-   img = Image.open("sample.jpg") 
+   img = Image.open("./assets/sample") 
    #print(np.array(img)[:,:,0])
    #plt.imshow(np.array(img)[:,:,0], cmap="gray")
    return np.array(img)[:,:,0]
@@ -35,20 +41,18 @@ def image_processing():
 ### 1   1   1
 ### 1   1   1
 ### 1   1   1
-def Mean_Blur(kernel = 16):
+def Mean_Blur(kernel = 32) -> None:
     image = image_processing()
     print(image.shape)
     image_size, blured_image = conv(image, kernel)
     new_image = np.array(blured_image).reshape(image_size, image_size)
-    #print(new_image)
-    plt.imshow(new_image, cmap = "gray")
+    plt.imshow(new_image, cmap='brg')
     plt.show()
 
 ### 1   2   1
 ### 2   4   2
 ### 1   2   1
-def Gaussian(kernel = 3):
-    pass
 
-Mean_Blur()
+if __name__ == "__main__":
+    Mean_Blur()
 
